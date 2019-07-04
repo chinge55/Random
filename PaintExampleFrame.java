8c6642fb5b40c005bd482ce75d67414db8e8c266
sangam <sangamkhnl@gmail.com>
2019-07-04 12:05:48 -0400

    Minor changes

--- PaintExampleFrame.java
+++ PaintExampleFrame.java
@@ -2,9 +2,12 @@ import java.util.concurrent.TimeUnit;
 import javax.swing.*;
 import java.awt.*;
 import java.io.*;
-
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 class PaintExampleFrame extends JFrame
 {
+
+	static int points_arr[][] = new int[501][501];
 	PaintExampleFrame()
 	{	
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -24,16 +27,34 @@ class PaintExampleFrame extends JFrame
 		while(true)
 		{
 			i++;
+
 			g.setColor(Color.red);
-			//int a = Math.abs(Random.t1ran-x);
-			//int b = Math.abs(Random.t2ran-y);
 			x = Random.t1ran;
 			y = Random.t2ran;
+			
+			points_arr[x][y]= points_arr[x][y]+1;
+
+			switch(points_arr[x][y])
+			{
+				case 2:
+					g.setColor(Color.blue);
+					break;
+				case 3:
+					g.setColor(Color.green);
+					break;
+				case 4:
+					g.setColor(Color.yellow);
+					break;
+			
+			}
+			//int a = Math.abs(Random.t1ran-x);
+			//int b = Math.abs(Random.t2ran-y);
 			fw.write(x+","+y+"\n");
-			System.out.println("x="+x+"y="+y);
+			
+			System.out.println("x="+x+", y="+y+" Occurences:"+points_arr[x][y]);
 			g.drawLine(x,y,x,y);
 			try{
-				
+			
 				TimeUnit.MILLISECONDS.sleep(10);
 			}
 			catch(Exception e)
@@ -41,7 +62,19 @@ class PaintExampleFrame extends JFrame
 			
 			}
 			if(i==1000)
-				break;
+			{	
+				try
+				{
+					BufferedImage image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+					ImageIO.write(image, "png", new File("./screenshot.png"));
+					break;
+				}
+				catch(Exception e)
+				{
+					e.printStackTrace();
+					break;
+				}
+			}
 		}
 	fw.close();
 	}catch(IOException e)
